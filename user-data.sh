76abb6fc43dce0e601819d0dfc330196e2877caa
carlosival <carlosmartinezival@gmail.com>
2017-07-27 13:34:24 +0200

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -2,14 +2,14 @@
 #Se usa set para ver la ejecucion del script linea por linea. en caso de ayuda
 #set -e -x
 #El id de la instancia que corre el script automatizar este paso.
-instance_ids="i-08a58a649478a4f81"
+instance_ids="i-08x58x4x478x1"
 # El /etc/profile se declaran las variables accesibles para todos los usuarios del sistema.  
 expose_file="/etc/profile.d/expose.sh"
 # Fichero para guardar los log o salidas del script
 log_file="/opt/init/ec2/user-data.log"
 # Comando que obtiene la ip publica  de la instancia que cambia en cada stop y start de la aplicacion
 ip_public=$(aws ec2 describe-instances --instance-ids $instance_ids --query 'Reservations[*].Instances[*].PublicIpAddress' --region 'us$
-dns_public_name=$(aws ec2 describe-instances --instance-id i-08a58a649478a4f81 --query 'Reservations[*].Instances[*].PublicDnsName' --r$
+dns_public_name=$(aws ec2 describe-instances --instance-id i-0xa5x64xx8a4x1 --query 'Reservations[*].Instances[*].PublicDnsName' --r$
 #Crear el fichero donde estan los comandos para exportar las variables hacia /etc/profile
 touch $expose_file
 chmod +x $expose_file
